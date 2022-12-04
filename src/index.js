9aef3b209180e7a3efccdecbc380118b5bd97ab3
berkay sonel <berkaysonel85@gmail.com>
2022-12-04 22:17:03 +0300

    Active and unactive feature added to navigation items.

--- src/index.js
+++ src/index.js
@@ -24,6 +24,14 @@ function createNavMenu() {
     const contact_button = document.createElement("a");
     contact_button.textContent = "Contact";
 
+    home_button.setAttribute("class", "page-link active");
+    menu_button.setAttribute("class", "page-link");
+    contact_button.setAttribute("class", "page-link");
+
+    home_button.setAttribute("id", "Home");
+    menu_button.setAttribute("id", "Menu");
+    contact_button.setAttribute("id", "Contact");
+
     nav_menu.appendChild(home_button);
     nav_menu.appendChild(menu_button);
     nav_menu.appendChild(contact_button);
@@ -38,10 +46,22 @@ function createMain() {
     return main;
 }
 
-function createFooter() {
-    // add footer later
-    // return footer;
-};
+// function createFooter() {
+//     // add footer later
+//     // return footer;
+// };
+
+function activePage(link_ID) {
+    const page_links = document.querySelectorAll(".page-link");
+    page_links.forEach((link) => {
+        if (link_ID != link.id) {
+            link.classList.remove("active");
+        }
+        else if (link_ID == link.id) {
+            link.classList.add("class", "active");
+        }
+    })
+}
 
 function initWebsite(){
     const content = document.getElementById("content");
@@ -50,7 +70,15 @@ function initWebsite(){
     content.appendChild(createMain());
     // content.appendChild(createFooter());
 
+    document.querySelectorAll(".page-link").forEach((link) => {
+        link.addEventListener("click",(e) => {
+            if(e.target.classList.contains("active")) return;
+            activePage(e.target.textContent)
+        });
+    });
+
     loadHome();
+
 }
 
 
